c22fd9e3b8090c37d5f161bd4614ef1ed354161d
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-02-02 00:24:33 +0530

    Trying to modify the login section

--- handlelogin.php
+++ handlelogin.php
@@ -1,34 +1,28 @@
-<html>
+<?php
+    $conn = mysqli_connect("localhost", "root", "", "registration" );
+?>
+   <html>
     <body>
        <center>
         <?php
         $email = $_POST['email'];
         $pwd = $_POST['pwd'];
-        $sql="select * from registration where email = '$email' and password = '$pwd' ";
-            
-            $dbServername = "localhost";
-            $doUsername = "root";
-        $dbPassword = "";
-        $dbName = "registration";
         
-        $conn = mysqli_connect($dbServername,$doUsername,$dbPassword,$dbName);
-           if(!$conn){
-               die("Connection failed: ".mysqli_connect_error());
+        
+           if(isset($_POST["Login"])){
+               $email = $_POST['email'];
+               $pwd = $_POST['pwd'];
+               $query = "select * from details where email='$email' && password='$pwd'";
+               $result = mysqli_query($query, $conn);
+               if(mysqli_num_rows($result)<=0){
+                   ?>
+                   <p>Invalid Username</p>
+                   <a href="index.php">Click to go back</a>
+                   <?php
+               }
+               else
+                    echo "Authentication Successfully";    
            }
-        $db = null;
-        $db = mysql_select_db($dbName,$conn); 
-        
-        $resultval = mysql_query($sql,$conn);
-        if(mysql_num_rown($resultval) <=0){
-            ?>
-            <br><p style="color:red">Invalid Username or Password</p><br>
-            <a href="index.php">Click to go back</a>
-            <?php
-        }
-        
-            else
-                    echo "<h1>Authenticated successfully.</h1>";
-            mysql_close($conn);
             ?>
         </center>
     </body>
