94d126cabc855800bf25c5faf06781fa857d128c
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-02-03 00:12:51 +0530

    Modefied Login Section

--- handlelogin.php
+++ handlelogin.php
@@ -5,25 +5,30 @@
     <body>
        <center>
         <?php
-        $email = $_POST['email'];
-        $pwd = $_POST['pwd'];
+            if(isset($_POST['Login'])){
+                $email = $_POST['email'];
+                $pass = $_POST['pwd'];
+                
+                $sql = "select * from details where email='".$email."' AND password='".$pass."'";
+                $result=mysqli_query($conn, $sql);
+                
+                if(mysqli_num_rows($result)==1){
+                    echo "You are successfully logged in ";
+                    exit();
+                }
+                else{
+                    echo "Wrong email or password";
+                    ?>
+                    <br><a href="index.php">Click to go back.</a>
+                    
+                    
+                    
+                    <?php
+                    exit();
+                }
+            }
         
-        
-           if(isset($_POST["Login"])){
-               $email = $_POST['email'];
-               $pwd = $_POST['pwd'];
-               $query = "select * from details where email='$email' && password='$pwd'";
-               $result = mysqli_query($query, $conn);
-               if(mysqli_num_rows($result)<=0){
-                   ?>
-                   <p>Invalid Username</p>
-                   <a href="index.php">Click to go back</a>
-                   <?php
-               }
-               else
-                    echo "Authentication Successfully";    
-           }
-            ?>
+           ?>
         </center>
     </body>
 </html>
